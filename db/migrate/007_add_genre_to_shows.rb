1adebf1c9743b89b98a988f5e0c72d55103da7ae
Christopher Tompkins <chtompkins026@gmail.com>
2018-11-21 21:53:04 +0000

    automatically backed up by learn

--- db/migrate/007_add_genre_to_shows.rb
+++ db/migrate/007_add_genre_to_shows.rb
@@ -1,12 +1,5 @@
 class AddGenreToShows < ActiveRecord::Migration[4.2]
   def change
     add_column :shows, :genre, :string
-<<<<<<< HEAD
   end
 end
-=======
-    add_column :shows, :day, :string 
-    add_column :shows, :season, :string 
-  end
-end
->>>>>>> 894442ccc5388c3b61f480e36ded41dfe3169262
